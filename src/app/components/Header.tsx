d03cad1a1486621c8c5bbf714b267b0171769890
pipidepulus <pipidepulus@hotmail.com>
2024-04-15 18:11:30 -0500

    Insert logo1

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Header({ className }: { className?: string }) {
   return (
     <header className={`flex items-center justify-center text-gray-200 text-2xl ${className}`}>
-      <Image src="/logo.png" alt="Logo" width={50} height={50} />
+      <Image src="/pinecone.svg" alt="Logo" width={50} height={50} />
     </header>
   );
 }
