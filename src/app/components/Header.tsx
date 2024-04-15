412e01e1ab51d54d6b68890498c673c5300d82ba
pipidepulus <pipidepulus@hotmail.com>
2024-04-15 17:17:25 -0500

    commit check connection 2

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,3 @@
-import Image from "next/image";
-import PineconeLogo from "../../../public/pinecone.svg";
-import VercelLogo from "../../../public/vercel.svg";
-
 export default function Header({ className }: { className?: string }) {
-  return (
-    <header
-      className={`flex items-center justify-center text-gray-200 text-2xl ${className}`}
-    >
-     
-    </header>
-  );
+  return null;
 }
