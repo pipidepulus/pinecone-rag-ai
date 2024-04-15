add71714f988b471e946057d4d0d103b574bd2ae
pipidepulus <pipidepulus@hotmail.com>
2024-04-15 18:16:17 -0500

    Insert logo2

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image';
 export default function Header({ className }: { className?: string }) {
   return (
     <header className={`flex items-center justify-center text-gray-200 text-2xl ${className}`}>
+      <span>A RAG AI bot made with </span>
       <Image src="/pinecone.svg" alt="Logo" width={50} height={50} />
+      <span> @pipidepulus</span>
     </header>
   );
 }
