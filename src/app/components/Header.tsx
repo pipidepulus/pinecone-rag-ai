2a08b9dc18058e0374916a961c7cc1608ea2883d
pipidepulus <pipidepulus@hotmail.com>
2024-04-15 18:06:26 -0500

    Insert logo

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,3 +1,9 @@
+import Image from 'next/image';
+
 export default function Header({ className }: { className?: string }) {
-  return null;
+  return (
+    <header className={`flex items-center justify-center text-gray-200 text-2xl ${className}`}>
+      <Image src="/logo.png" alt="Logo" width={50} height={50} />
+    </header>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,12 +51,7 @@ const Page: React.FC = () => {
   return (
     <div className="flex flex-col justify-between h-screen bg-gray-800 p-2 mx-auto max-w-full">
       <Header className="my-5" />
-      
-
-      
-
-      
-
+   
       <InstructionModal
         isOpen={isModalOpen}
         onClose={() => setModalOpen(false)}
